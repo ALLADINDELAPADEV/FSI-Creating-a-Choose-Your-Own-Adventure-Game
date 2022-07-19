84971b28a25b028d537cdeb7dd0156b68ecbbd94
ALLADINDELAPADEV <104471458+ALLADINDELAPADEV@users.noreply.github.com>
2022-07-19 10:41:30 -0700

    just added code to start the adventure

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 //Your Code here
+
+let response = window.prompt('do you go left or right?')
+
+console.log(response)
+
+if(response === "left"){
+
+console.log("you follow a darken path")
+
+
+}
+
+else if(response === "right"){
+
+    console.log("you follow a less forbodding path")
+
+
+
+
+}
+
+else
+
+{
+
+   console.log("you hvae to choose becuase you hear something sinister behind you")
+
+}
